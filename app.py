1d2cc76043e77c614f588234de4467d4e3ce59d1
Ceeq <Juwennagpal6@gmail.com>
2023-06-14 19:22:00 +0200

    added some html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 
 from router import router
 
@@ -9,6 +10,26 @@ app = FastAPI()
 app.include_router(
     router
 )
+
+@app.get("/")
+async def root(request: Request) -> HTMLResponse:
+    return HTMLResponse(
+        content="""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo RestAPI</title>
+</head>
+<body>
+    please visit <a href="http://localhost:8080/docs">docs</a>
+    <p>This site is not finished yet!</p>
+</body>
+</html>
+        """
+    )
 
 if __name__ == "__main__":
     uvicorn.run(
